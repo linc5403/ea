f93a0cea3d47a7f8824ad3b3ed71e10034b48580
lin chuan <linch1982@gmail.com>
2020-04-24 16:58:03 +0800

    增加了Controller的单元测试

--- 03-unit-test-demo/src/main/java/club/banyuan/unitTestDemo/IndexController.java
+++ 03-unit-test-demo/src/main/java/club/banyuan/unitTestDemo/IndexController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@RequestMapping("")
+@Controller("")
+@RequestMapping
 public class IndexController {
     @GetMapping
     @ResponseBody
---
+++ 03-unit-test-demo/src/test/java/club/banyuan/unitTestDemo/IndexControllerTest.java
@@ -0,0 +1,33 @@
+package club.banyuan.unitTestDemo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class IndexControllerTest {
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        this.mvc = MockMvcBuilders.standaloneSetup(new IndexController()).build();
+    }
+    @Test
+    public void showHomePage() throws Exception {
+        this.mvc.perform(
+                    get("/")
+                        .accept(MediaType.parseMediaType("application/html;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/html;charset=UTF-8"))
+                .andExpect(content().string("Hello"));
+    }
+}
