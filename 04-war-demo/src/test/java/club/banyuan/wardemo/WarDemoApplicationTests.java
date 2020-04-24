0eb840a823e0ba18ad33acce249a8cc52d1865ff
lin chuan <linch1982@gmail.com>
2020-04-24 10:55:44 +0800

    添加单元测试和war的demo

---
+++ 04-war-demo/src/test/java/club/banyuan/wardemo/WarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package club.banyuan.wardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
